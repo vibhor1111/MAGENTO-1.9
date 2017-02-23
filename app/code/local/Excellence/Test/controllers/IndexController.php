83cc1fe2c1f567b1341312d27a8c0474d1a63d98
etech <bijit@excellencetechnologies.in>
2017-02-23 17:14:13 +0530

    changed echoing table row and table data using PHP, changed model name and module name

--- app/code/local/Excellence/Test/controllers/IndexController.php
+++ app/code/local/Excellence/Test/controllers/IndexController.php
@@ -3,12 +3,12 @@ class Excellence_User_IndexController extends Mage_Core_Controller_Front_Action
 {
     public function deleteAction()
     {   
-        if( $this->getRequest()->getParam('id') > 0 ) {
+        if( $this->getRequest()->getParam('user_id') > 0 ) {
          try {
-              $model = Mage::getModel('user/user');
-              $model2 = Mage::getModel('user/address');
-              $model->setId($this->getRequest()->getParam('id'))->delete();
-              $model2->setId($this->getRequest()->getParam('id'))->delete();
+              $user_model = Mage::getModel('user/user');
+              $address_model = Mage::getModel('user/address');
+              $user_model->setId($this->getRequest()->getParam('user_id'))->delete();
+              $address_model->setId($this->getRequest()->getParam('user_id'))->delete();
               Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('adminhtml')->__('Item was successfully deleted'));
               $this->_redirect('*/*/index');
              } 
@@ -20,9 +20,12 @@ class Excellence_User_IndexController extends Mage_Core_Controller_Front_Action
       }
   public function saveAction() {
     $fields = $this->getRequest()->getParams();
-    $model = Mage::getModel("user/user");
-    $model->setData($fields);
-    $model->save();
+    $user_model = Mage::getModel("user/user");
+    $address_model = Mage::getModel("user/address");
+    $user_model->setData($fields);
+    $address_model->setData($fields);
+    $user_model->save();
+    $address_model->save();
     $this->_redirect('user/index/index');
   }
   public function indexAction()
