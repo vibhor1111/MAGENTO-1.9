ca58f6c41f0b85d9ee5e7b535c76627c5ba5c241
vibhor <vibhor@excellencetechnologies.in>
2017-02-23 15:18:49 +0530

    changed variable name model and model2

--- app/code/local/Excellence/User/Block/User.php
+++ app/code/local/Excellence/User/Block/User.php
@@ -1,13 +1,6 @@
 <?php
 class Excellence_User_Block_User extends Mage_Core_Block_Template
 {
-    public function getContent()
-    {
-        return "ITS WORKING";
-
-      echo "<br>";
-
-    }
-
+    
 }
 ?>
--- app/code/local/Excellence/User/controllers/IndexController.php
+++ app/code/local/Excellence/User/controllers/IndexController.php
@@ -5,27 +5,11 @@ class Excellence_User_IndexController extends Mage_Core_Controller_Front_Action
     {     
        if( $this->getRequest()->getParam('id') > 0 ) {
          try {
-              $model = Mage::getModel('user/user');
-              $model2 = Mage::getModel('user/address');
+              $user_model = Mage::getModel('user/user');
+              $address_model = Mage::getModel('user/address');
 
-              $model->setId($this->getRequest()->getParam('id'))->delete();
-              $model2->setId($this->getRequest()->getParam('id'))->delete();
-
-             Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('adminhtml')->__('Item was successfully deleted'));
-             $this->_redirect('*/*/');
-            } 
-         catch (Exception $e) {
-            Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
-            $this->_redirect('*/*/');
-         }
-        }
-
-        /*================================Added for deleting table 2==================================*/
-        if( $this->getRequest()->getParam('id2') > 0 ) {
-         try {
-              $model = Mage::getModel('user/address');
-
-              $model->setId($this->getRequest()->getParam('id2'))->delete();
+              $user_model->setId($this->getRequest()->getParam('id'))->delete();
+              $address_model->setId($this->getRequest()->getParam('id'))->delete();
 
              Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('adminhtml')->__('Item was successfully deleted'));
              $this->_redirect('*/*/');
@@ -35,24 +19,24 @@ class Excellence_User_IndexController extends Mage_Core_Controller_Front_Action
             $this->_redirect('*/*/');
          }
         }
+    }
+
+    public function saveAction()
+     {
+        $fields = $this->getRequest()->getParams();
+        $user_model = Mage::getModel("user/user");
+        $address_model = Mage::getModel("user/address");
+        $user_model->setData($fields);
+        $address_model->setData($fields);
+        $user_model->save();
+        $address_model->save();
+        $this->_redirect('user/index/index');
      }
-public function saveAction() {
-  $fields = $this->getRequest()->getParams();
-  $model = Mage::getModel('user/user')->getCollection();
-     $model = Mage::getModel("user/user");
-     $model->setData($fields);
-     $model->save();
-     $this->_redirect('*/*/');
-    
-    // }
-}
- /*===================================save action for second table===================================*/
- 
+
  public function indexAction() 
     { 
         $this->loadLayout();            
         $this->renderLayout();
-    }
- /*========================================================================================*/
+   }
  
 }
