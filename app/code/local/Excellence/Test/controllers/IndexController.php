35a6ea6492ff30dd28d102206ce1d5ca36b9c32d
etech <bijit@excellencetechnologies.in>
2017-02-23 10:46:35 +0530

    new indexcontroller file, having saveAction() and deleteAction()

--- app/code/local/Excellence/Test/controllers/IndexController.php
+++ app/code/local/Excellence/Test/controllers/IndexController.php
@@ -1,7 +1,7 @@
 <?php
 class Excellence_Test_IndexController extends Mage_Core_Controller_Front_Action
 {
-    public function indexAction()
+    public function deleteAction()
     { 
         $this->loadLayout();            
         $this->renderLayout();    
@@ -19,102 +19,18 @@ class Excellence_Test_IndexController extends Mage_Core_Controller_Front_Action
             $this->_redirect('*/*/');
          }
         }
-
-        /*================================Added for deleting table 2==================================*/
-        if( $this->getRequest()->getParam('id2') > 0 ) {
-         try {
-              $model = Mage::getModel('test/test2');
-
-              $model->setId($this->getRequest()->getParam('id2'))->delete();
-
-             Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('adminhtml')->__('Item was successfully deleted'));
-             $this->_redirect('*/*/index');
-            } 
-         catch (Exception $e) {
-            Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
-            $this->_redirect('*/*/');
-         }
-        }
-        /*=============================================================================================*/
-
       }
- public function saveAction() {
-  $id = $this->getRequest()->getParam('testid');
-  $title = $this->getRequest()->getParam('title');
-  $filename = $this->getRequest()->getParam('filename');
-  $content = $this->getRequest()->getParam('content');
-  $status = $this->getRequest()->getParam('status');
-  $fields = $this->getRequest()->getParams();
-  $indicator = 0;
-  $model = Mage::getModel('test/test')->getCollection();
-  foreach($model as $data)
+  public function saveAction() {
+    $fields = $this->getRequest()->getParams();
+    $model = Mage::getModel("user/user");
+    $model->setData($fields);
+    $model->save();
+    $this->_redirect('user/index/index');
+  }
+  public function indexAction()
    {
-    $existing_id = $data->getTestId();
-    if($id == $existing_id)
-     {
-      $indicator = 1;
-     }
+       $this->loadLayout();            
+       $this->renderLayout();
    }
-  if($indicator == 1)
-    {
-     $model = Mage::getModel('test/test')->load($id);
-     $model->setTitle($title);
-     $model->setFilename($filename);
-     $model->setContent($content);
-     $model->setStatus($status);
-     $model->setUpdateTime(strtotime('now'));
-     $model->save();
-     $this->_redirect('test/index/index');
-    }
-   else
-    {
-     $model = Mage::getModel("test/test");
-     $model->setData($fields);
-     $model->setCreatedTime(strtotime('now'));
-     $model->save();
-      $this->_redirect('test/index/index');
-    }
-}
-
- /*===================================save action for second table===================================*/
- public function save2Action() 
- {  
-  $id = $this->getRequest()->getParam('testid');
-  $title = $this->getRequest()->getParam('title');
-  $filename = $this->getRequest()->getParam('filename');
-  $content = $this->getRequest()->getParam('content');
-  $status = $this->getRequest()->getParam('status');
-  $fields = $this->getRequest()->getParams();
-  $indicator = 0;
-  $model = Mage::getModel('test/test2')->getCollection();
-  foreach($model as $data)
-   {
-    $existing_id = $data->getTestId();
-    if($id == $existing_id)
-     {
-      $indicator = 1;
-     }
-   }
-  if($indicator == 1)
-    {
-     $model = Mage::getModel('test/test2')->load($id);
-     $model->setTitle($title);
-     $model->setFilename($filename);
-     $model->setContent($content);
-     $model->setStatus($status);
-     $model->setUpdateTime(strtotime('now'));
-     $model->save();
-     $this->_redirect('test/index/index');
-    }
-   else
-    {
-     $model = Mage::getModel("test/test2");
-     $model->setData($fields);
-     $model->setCreatedTime(strtotime('now'));
-     $model->save();
-      $this->_redirect('test/index/index');
-    }
- }
- /*========================================================================================*/
 
 }
