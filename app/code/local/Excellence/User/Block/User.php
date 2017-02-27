eac4eff3e0740722299c83bac5c9c2b522905ce8
vibhor <vibhor@excellencetechnologies.in>
2017-02-27 11:35:12 +0530

    call coleection in Block File

--- app/code/local/Excellence/User/Block/User.php
+++ app/code/local/Excellence/User/Block/User.php
@@ -1,6 +1,16 @@
 <?php
 class Excellence_User_Block_User extends Mage_Core_Block_Template
 {
-    
+ 
+    public function getUserCollection()
+	{
+		return Mage::getModel('user/user')->getCollection();
+	}
+
+	public function getAddressCollection()
+	{
+		return Mage::getModel('user/address')->getCollection();
+	}
+
 }
 ?>
