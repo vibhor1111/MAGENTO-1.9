72d7bad5c4467355719f6087ac786ce34122ec9d
etech <bijit@excellencetechnologies.in>
2017-02-27 17:06:37 +0530

    separated delete and save code from indexController.php and moved to model file inside a function

--- app/code/local/Excellence/User/controllers/IndexController.php
+++ app/code/local/Excellence/User/controllers/IndexController.php
@@ -1,40 +1,26 @@
 <?php
 class Excellence_User_IndexController extends Mage_Core_Controller_Front_Action
 {
+ public function saveAction() 
+  {
+   $fields = $this->getRequest()->getParams();
+   $user_model = Mage::getModel("user/user");
+   $user_model->saveUser($fields); //calling saveUser() in user model for save operation in user table
+   $user_id = $user_model->getUserId();
+   $address_model = Mage::getModel("user/address");
+   $address_model->saveUser($fields, $user_id); //calling saveUser() in address model for save operation in address table
+   $this->_redirect('*/*/index');
+  }
  public function deleteAction()
   {   
-   if( $this->getRequest()->getParam('user_id') > 0 )
-    {
-     $user_id = $this->getRequest()->getParam('user_id');
-     $user_model = Mage::getModel('user/user');
-     $user_model->setId($this->getRequest()->getParam('user_id'))->delete(); //passing user id to delete data from user table
-     $address_model = Mage::getModel('user/address')->getCollection();
-     foreach($address_model as $data)
-      {
-       $address_user_id = $data->getUserid();
-       if ($address_user_id == $user_id) // used to identify common user id of user table and address table
-        {
-         $address_id = $data->getAddressId(); //passing address id to delete data from address table
-         $data->setId($address_id)->delete();
-        }
-      }
-     $this->_redirect('*/*/index');
-    }
+   $user_model = Mage::getModel('user/user');
+   $user_model->deleteUser($this->getRequest()->getParam('user_id')); //calling deleteUser() in user model for delete operation in user table
+   $fields = $this->getRequest()->getParams();
+   $address_model = Mage::getModel('user/address');
+   $address_model->deleteUser($this->getRequest()->getParam('user_id')); //calling deleteUser() in address model for delete operation in address table
+   $this->_redirect('*/*/index');
   }
-  public function saveAction() 
-   {
-    $fields = $this->getRequest()->getParams();
-    $user_model = Mage::getModel("user/user");
-    $address_model = Mage::getModel("user/address");
-    $user_model->setData($fields);
-    $address_model->setData($fields);
-    $user_model->save();
-    $user_id = $user_model->getUserId();
-    $address_model->setUserid($user_id);
-    $address_model->save();
-    $this->_redirect('user/index/index');
-   }
-  public function indexAction()
+ public function indexAction()
    {
        $this->loadLayout();            
        $this->renderLayout();
