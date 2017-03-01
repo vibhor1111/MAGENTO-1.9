125327bb4af50ef179b50828b364d0a00aee5c90
etech <bijit@excellencetechnologies.in>
2017-03-01 16:14:27 +0530

    pagination

--- app/code/local/Excellence/User/Block/User.php
+++ app/code/local/Excellence/User/Block/User.php
@@ -1,16 +1,33 @@
 <?php
 class Excellence_User_Block_User extends Mage_Core_Block_Template
 {
- 
+    public function __construct()
+    {
+        parent::__construct();
+        $collection = Mage::getModel('user/user')->getCollection();
+        $this->setCollection($collection);
+    }
+    protected function _prepareLayout()
+    {
+        parent::_prepareLayout();
+        $pager = $this->getLayout()->createBlock('page/html_pager', 'custom.pager');
+        $pager->setAvailableLimit(array(5=>5,10=>10,20=>20,'all'=>'all'));
+        $pager->setCollection($this->getCollection());
+        $this->setChild('pager', $pager);
+        $this->getCollection()->load();
+        return $this;
+    }
+    public function getPagerHtml()
+    {
+        return $this->getChildHtml('pager');
+    }
     public function getUserCollection()
 	{
 		return Mage::getModel('user/user')->getCollection();
 	}
-
 	public function getAddressCollection()
 	{
 		return Mage::getModel('user/address')->getCollection();
 	}
-
 }
 ?>
