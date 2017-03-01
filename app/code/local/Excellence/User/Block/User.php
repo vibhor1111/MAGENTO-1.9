b2f01f417abf39157cf31149f7922252a67d1312
vibhor <vibhor@excellencetechnologies.in>
2017-03-01 17:13:13 +0530

    sorting of coloumns

--- app/code/local/Excellence/User/Block/User.php
+++ app/code/local/Excellence/User/Block/User.php
@@ -1,6 +1,56 @@
 <?php
 class Excellence_User_Block_User extends Mage_Core_Block_Template
-{
+{    
+	  public function __construct()
+    {
+        parent::__construct();
+        $collection = Mage::getModel('user/user')->getCollection();
+        $this->setCollection($collection);
+    }
+ 
+    protected function _prepareLayout()
+    {
+        parent::_prepareLayout();
+        $toolbar = $this->getToolbarBlock();
+        $collection = $this->getCollection();
+       	if ($orders = $this->getAvailableOrders()) {
+            $toolbar->setAvailableOrders($orders);
+        }
+        if ($sort = $this->getSortBy()) {
+            $toolbar->setDefaultOrder($sort);
+        }
+        if ($dir = $this->getDefaultDirection()) {
+            $toolbar->setDefaultDirection($dir);
+        }
+        $toolbar->setCollection($collection);
+        $this->setChild('toolbar', $toolbar);
+        $this->getCollection()->load();
+        return $this;
+	}
+        public function getDefaultDirection(){
+        return 'asc';
+          }
+    public function getAvailableOrders(){
+        return array('user_id'=>'ID','firstname'=>'firstname','lastname'=>'lastname','DOB'=>'dob');
+    }
+    public function getSortBy(){
+        return 'user_id';
+    }
+    public function getToolbarBlock()
+    {
+        $block = $this->getLayout()->createBlock('user/toolbar', microtime());
+        return $block;
+    }
+    public function getMode()
+    {
+        return $this->getChild('toolbar')->getCurrentMode();
+    }
+ 
+    public function getToolbarHtml()
+    {
+        return $this->getChildHtml('toolbar');
+    }
+ 
  
     public function getUserCollection()
 	{
