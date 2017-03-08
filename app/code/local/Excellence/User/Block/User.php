782c92f012ba5ef77201cf7265218912f8e491df
vibhor <vibhor@excellencetechnologies.in>
2017-03-08 10:35:30 +0530

    magento event handler

--- app/code/local/Excellence/User/Block/User.php
+++ app/code/local/Excellence/User/Block/User.php
@@ -32,52 +32,56 @@ class Excellence_User_Block_User extends Mage_Core_Block_Template
 	}
         public function getDefaultDirection()
       {
-        return 'asc';
+          return 'asc';
       }
         public function getAvailableOrders()
       {
-        return array('user_id'=>'ID','firstname'=>'firstname','lastname'=>'lastname','DOB'=>'dob');
+          return array('user_id'=>'ID','firstname'=>'firstname','lastname'=>'lastname','DOB'=>'dob');
       }
         public function getSortBy()
       {
-        return 'user_id';
+          return 'user_id';
       }
         public function getToolbarBlock()
       {
-        $block = $this->getLayout()->createBlock('user/toolbar', microtime());
-        return $block;
+          $block = $this->getLayout()->createBlock('user/toolbar', microtime());
+          return $block;
       }
         public function getMode()
       {
-        return $this->getChild('toolbar')->getCurrentMode();
+          return $this->getChild('toolbar')->getCurrentMode();
       }
         public function getPagerHtml()
       {
-        return $this->getChildHtml('pager');
+          return $this->getChildHtml('pager');
       }
         public function getToolbarHtml()
       {
-        return $this->getChildHtml('toolbar');
+          return $this->getChildHtml('toolbar');
       }
         public function getUserCollection()
-	  {
-		return Mage::getModel('user/user')->getCollection();
-	  }
-    	public function getAddressCollection()
-	  {
-		return Mage::getModel('user/address')->getCollection();
-	  }
+	    {
+		      return Mage::getModel('user/user')->getCollection();
+	    }
+    	  public function getAddressCollection()
+	    {
+		      return Mage::getModel('user/address')->getCollection();
+	    }
         public function getmultiselectCollection()
       {
-           return Mage::getStoreConfig('usersection/user/view_style',Mage::app()->getStore());
+          return Mage::getStoreConfig('usersection/user/view_style',Mage::app()->getStore());
       }
         public function gettextdataCollection()
       {
-           return Mage::getStoreConfig('usersection/user/patient',Mage::app()->getStore());
+          return Mage::getStoreConfig('usersection/user/patient',Mage::app()->getStore());
       }
         public function getdropdowndataCollection()
       {
-           return Mage::getStoreConfig('usersection/user/slider',Mage::app()->getStore());
+          return Mage::getStoreConfig('usersection/user/slider',Mage::app()->getStore());
+      }
+        public function getObserverCollection()
+      {
+          return Mage::getModel("user/observer")->getCollection();
       }
 
 }
---
+++ app/code/local/Excellence/User/controllers/EventController.php
@@ -0,0 +1,10 @@
+<?php
+ class Excellence_User_EventController extends Mage_Core_Controller_Front_Action
+  {
+   public function handlerAction()
+    {
+     $this->loadLayout();            
+     $this->renderLayout();
+    }
+  }
+?>
