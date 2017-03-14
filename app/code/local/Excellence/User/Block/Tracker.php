49155179351c455e21fd305cd9defaeef6c5e544
etech <bijit@excellencetechnologies.in>
2017-03-14 12:14:07 +0530

    storing customer details

--- app/code/local/Excellence/User/Block/Tracker.php
+++
@@ -1,9 +0,0 @@
-<?php
-class User_Tracker_Block_Tracker extends Mage_Core_Block_Template
- {
-    public function getContent()
-    {
-      return Mage::getModel("tracker/observer")->getCollection();
-    }
- }
-?>
