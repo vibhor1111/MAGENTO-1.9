7616c54f0f345cdf5fb7e3bf32f003c345c879ce
etech <bijit@excellencetechnologies.in>
2017-03-14 12:24:38 +0530

    commit

--- app/code/local/Excellence/User/Model/Mysql4/Observer2.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Excellence_User_Model_Mysql4_Observer2 extends Mage_Core_Model_Mysql4_Abstract
-{
-    public function _construct()
-    {   
-        $this->_init('user/observer2', 'observer_id');
-    }
-}
-?>
